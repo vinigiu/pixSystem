7a2d9f52a39511ef423caeaf4898cb599ae39b35
vinigiu <vinigiu@hotmail.com>
2022-08-26 19:11:53 -0300

    Adicionando funcionalidade de transferência

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -85,9 +85,36 @@ const indexController = {
         })
     },
 
-
     transfer: async (req,res) => {
-        res.status(200).json('Receberá dados de transferência')
+        const valor = req.body.valor;
+        const chaveDebito = req.body.chaveDebito;
+        const chaveCredito = req.body.chaveCredito;
+
+        //Necessário implementar lógica para que seja usada para débito automaticamente a conta do usuário logado
+
+        const userDebito = await db.Chave.findOne({where:{chave:chaveDebito}});
+        const userCredito = await db.Chave.findOne({where:{chave:chaveCredito}});
+
+        const carteiraDebito = await db.Carteira.findOne({where:{usuarios_id:userDebito.usuarios_id}});
+        const carteiraCredito = await db.Carteira.findOne({where:{usuarios_id:userCredito.usuarios_id}});
+
+        const saldoCarteiraDebito = await carteiraDebito.saldo;
+        const saldoCarteiraCredito = await carteiraCredito.saldo;
+
+        const saldoFinalCarteiraDebito = saldoCarteiraDebito - valor;
+        const saldoFinalCarteiraCredito = saldoCarteiraCredito + valor;
+
+        if(saldoCarteiraDebito < valor ) {
+            return res.status(401).json({data:"Saldo insuficiente"})
+        }
+        await db.Carteira.update({saldo: saldoFinalCarteiraDebito},{where:{usuarios_id:userDebito.usuarios_id}})
+        await db.Carteira.update({saldo: saldoFinalCarteiraCredito},{where:{usuarios_id:userCredito.usuarios_id}})
+
+        res.status(200).json({
+            data: {
+                saldoAtual: saldoFinalCarteiraDebito
+            }
+        })
     },
 
     account: async (req,res) => {
